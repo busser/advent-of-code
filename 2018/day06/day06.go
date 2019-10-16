696ea02b03e7b8276a67513828f05d5f209d0a4d
Arthur Busser <arthur.busser@gmail.com>
2019-10-16 11:03:49 +0200

    Add solution for day 06 2018

---
+++ 2018/day06/day06.go
@@ -0,0 +1,169 @@
+package day06
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+// Coordinate represents a
+type Coordinate struct {
+	X, Y int
+}
+
+// String implements the Stringer interface.
+func (c Coordinate) String() string {
+	return fmt.Sprintf("(%d,%d)", c.X, c.Y)
+}
+
+// PartOne solves Part One of the puzzle.
+func PartOne(coords []Coordinate) int {
+	// Get range of X and Y values of coordinates.
+	minX, maxX, minY, maxY := coords[0].X, coords[0].X, coords[0].Y, coords[0].Y
+	for _, coord := range coords {
+		if coord.X < minX {
+			minX = coord.X
+		}
+		if coord.X > maxX {
+			maxX = coord.X
+		}
+		if coord.Y < minY {
+			minY = coord.Y
+		}
+		if coord.Y > maxY {
+			maxY = coord.Y
+		}
+	}
+
+	// Find area of each coordinate.
+	areaByCoordinate := make(map[Coordinate]int)
+	areaIsInfiniteByCoordinate := make(map[Coordinate]bool)
+
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			closest, tied := closestCoordinate(x, y, coords)
+			if tied {
+				continue
+			}
+
+			areaByCoordinate[closest]++
+			if x == minX || x == maxX || y == minX || y == maxY {
+				areaIsInfiniteByCoordinate[closest] = true
+			}
+		}
+	}
+
+	// Find largest area that isn't infinite.
+	var largestFiniteArea int
+	for _, coord := range coords {
+		if !areaIsInfiniteByCoordinate[coord] && areaByCoordinate[coord] > largestFiniteArea {
+			largestFiniteArea = areaByCoordinate[coord]
+		}
+	}
+
+	return largestFiniteArea
+}
+
+// PartTwo solves Part Two of the puzzle.
+func PartTwo(coords []Coordinate) int {
+	// Get range of X and Y values of coordinates.
+	minX, maxX, minY, maxY := coords[0].X, coords[0].X, coords[0].Y, coords[0].Y
+	for _, coord := range coords {
+		if coord.X < minX {
+			minX = coord.X
+		}
+		if coord.X > maxX {
+			maxX = coord.X
+		}
+		if coord.Y < minY {
+			minY = coord.Y
+		}
+		if coord.Y > maxY {
+			maxY = coord.Y
+		}
+	}
+
+	const maxTotalDistance = 10000
+
+	// Count locations which have a total distance to all coordinates of less
+	// than maxTotalDistance.
+	var count int
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			if sumDistances(x, y, coords) < maxTotalDistance {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+// Distance returns the Manhattan distance between (x, y) and c.
+func distance(x, y int, c Coordinate) int {
+	return abs(x-c.X) + abs(y-c.Y)
+}
+
+// abs returns the absolute value of n.
+func abs(n int) int {
+	if n > 0 {
+		return n
+	}
+	return -n
+}
+
+// closestCoordinate finds the coordinate closest to (x,y) and whether there
+// is a tie. In case of a tie, any coordinate may be returned.
+func closestCoordinate(x, y int, coords []Coordinate) (Coordinate, bool) {
+	closestCoord := coords[0]
+	minDistance := distance(x, y, closestCoord)
+	var tied bool
+
+	for _, coord := range coords[1:] {
+		d := distance(x, y, coord)
+		if d < minDistance {
+			closestCoord = coord
+			minDistance = d
+			tied = false
+		} else if d == minDistance {
+			tied = true
+		}
+	}
+
+	return closestCoord, tied
+}
+
+// sumDistances computes the sum of (x,y) to each coordinate.
+func sumDistances(x, y int, coords []Coordinate) int {
+	var sum int
+
+	for _, coord := range coords {
+		sum += distance(x, y, coord)
+	}
+
+	return sum
+}
+
+// ReadCoordinates reads a list of coordinates from r.
+func ReadCoordinates(r io.Reader) ([]Coordinate, error) {
+	var coords []Coordinate
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		var c Coordinate
+
+		_, err := fmt.Sscanf(scanner.Text(), "%d, %d", &c.X, &c.Y)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse text")
+		}
+
+		coords = append(coords, c)
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return coords, nil
+}
---
+++ 2018/day06/day06_test.go
@@ -0,0 +1,28 @@
+package day06_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/busser/advent-of-code/2018/day06"
+)
+
+func Example() {
+	file, err := os.Open("testdata/input.txt")
+	if err != nil {
+		log.Fatalf("failed to open input file: %v", err)
+	}
+	defer file.Close()
+
+	coords, err := day06.ReadCoordinates(file)
+	if err != nil {
+		log.Fatalf("failed to read coordinates: %v", err)
+	}
+
+	fmt.Printf("The size of the largest finite area is %d.\n", day06.PartOne(coords))
+	fmt.Printf("%d locations have a total distance to all given coordinates of less than 10000.\n", day06.PartTwo(coords))
+	// Output:
+	// The size of the largest finite area is 3260.
+	// 42535 locations have a total distance to all given coordinates of less than 10000.
+}
